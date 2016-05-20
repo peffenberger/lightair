72953d30ee945bfcae4bfffbe40622acfc9eebe9
Petr Effenberger <petr@effy.cz>
2016-05-20 07:39:57 +0200

    junit runner WIP

--- src/main/java/net/sf/lightair/internal/junit/SetupListTestRule.java
+++ src/main/java/net/sf/lightair/internal/junit/SetupListTestRule.java
@@ -1,24 +1,32 @@
 package net.sf.lightair.internal.junit;
 
-import net.sf.lightair.annotation.Setup;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.junit.runners.model.FrameworkMethod;
 
+import net.sf.lightair.annotation.Setup;
+
 /**
- * JUnit test rule to setup database with multiple <code>@Setup</code>
- * annotations before test method execution.
+ * JUnit test rule to setup database with multiple <code>@Setup</code> annotations before test method execution.
  */
 public class SetupListTestRule extends AbstractTestRule<Setup.List> {
 
+	private SetupExecutor setupExecutor;
+
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param frameworkMethod
 	 *            JUnit framework method on which the test rule is being applied
 	 */
 	public SetupListTestRule(FrameworkMethod frameworkMethod) {
 		super(frameworkMethod, Setup.List.class, Setup.class);
 	}
+
+	// beans and their setters:
 
 	/**
 	 * If the method is annotated with @{@link Setup}, set up the database.
@@ -27,15 +35,37 @@ public class SetupListTestRule extends AbstractTestRule<Setup.List> {
 	protected void before() {
 		if (null != getAnnotation()) {
 			Setup[] setups = getAnnotation().value();
-			for (Setup setup : setups) {
-				setupExecutor.execute(setup, getTestMethod());
+			Map<String, List<Setup>> setupMap = new HashMap<String, List<Setup>>();
+			for (Setup set : setups) {
+				String profile = set.profile();
+				if (setupMap.containsKey(profile)) {
+					setupMap.get(profile).add(set);
+				} else {
+					ArrayList<Setup> listForProfile = new ArrayList<Setup>();
+					listForProfile.add(set);
+					setupMap.put(profile, listForProfile);
+				}
+			}
+			ArrayList<Callable<Void>> callables = new ArrayList<Callable<Void>>();
+			for (final Map.Entry<String, List<Setup>> entry : setupMap.entrySet()) {
+				callables.add(new Callable<Void>() {
+					@Override
+					public Void call() throws Exception {
+						for (Setup set : entry.getValue()) {
+							setupExecutor.execute(set, getTestMethod());
+						}
+						return null;
+					}
+				});
+			}
+			ExecutorService execSvc = Executors.newCachedThreadPool();
+			try {
+				execSvc.invokeAll(callables);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 	}
-
-	// beans and their setters:
-
-	private SetupExecutor setupExecutor;
 
 	/**
 	 * Set setup executor.
--- src/main/java/net/sf/lightair/internal/junit/VerifyListTestRule.java
+++ src/main/java/net/sf/lightair/internal/junit/VerifyListTestRule.java
@@ -1,24 +1,35 @@
 package net.sf.lightair.internal.junit;
 
-import net.sf.lightair.annotation.Verify;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.junit.runners.model.FrameworkMethod;
 
+import net.sf.lightair.annotation.Verify;
+
 /**
- * JUnit test rule to verify database with multiple <code>@Verify</code>
- * annotations after test method execution.
+ * JUnit test rule to verify database with multiple <code>@Verify</code> annotations after test method execution.
  */
 public class VerifyListTestRule extends AbstractTestRule<Verify.List> {
 
+	private VerifyExecutor verifyExecutor;
+
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param frameworkMethod
 	 *            JUnit framework method on which the test rule is being applied
 	 */
 	public VerifyListTestRule(FrameworkMethod frameworkMethod) {
 		super(frameworkMethod, Verify.List.class, Verify.class);
 	}
+
+	// beans and their setters:
 
 	/**
 	 * If the method is annotated with @{@link Verify}, set up the database.
@@ -27,15 +38,37 @@ public class VerifyListTestRule extends AbstractTestRule<Verify.List> {
 	protected void after() {
 		if (null != getAnnotation()) {
 			Verify[] verifies = getAnnotation().value();
-			for (Verify verify : verifies) {
-				verifyExecutor.execute(verify, getTestMethod());
+			Map<String, List<Verify>> setupMap = new HashMap<String, List<Verify>>();
+			for (Verify set : verifies) {
+				String profile = set.profile();
+				if (setupMap.containsKey(profile)) {
+					setupMap.get(profile).add(set);
+				} else {
+					ArrayList<Verify> listForProfile = new ArrayList<Verify>();
+					listForProfile.add(set);
+					setupMap.put(profile, listForProfile);
+				}
+			}
+			ArrayList<Callable<Void>> callables = new ArrayList<Callable<Void>>();
+			for (final Map.Entry<String, List<Verify>> entry : setupMap.entrySet()) {
+				callables.add(new Callable<Void>() {
+					@Override
+					public Void call() throws Exception {
+						for (Verify set : entry.getValue()) {
+							verifyExecutor.execute(set, getTestMethod());
+						}
+						return null;
+					}
+				});
+			}
+			ExecutorService execSvc = Executors.newCachedThreadPool();
+			try {
+				execSvc.invokeAll(callables);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 	}
-
-	// beans and their setters:
-
-	private VerifyExecutor verifyExecutor;
 
 	/**
 	 * Set verify executor.
